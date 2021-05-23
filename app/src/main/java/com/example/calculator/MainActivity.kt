fe8fcb3972c5b115b600a2c4fcb84097268b4931
Constantkavaya <kavayaconstant@gmail.comgit config --listgit config --global user.email kavayaconstant@gmail.com>
2021-05-23 14:45:04 +0300

    first commit

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -17,42 +17,40 @@ class MainActivity : AppCompatActivity() {
         var answer = findViewById<TextView>(R.id.tvanswer)
         var btnsum = findViewById<Button>(R.id.btnsum)
         var btnminus = findViewById<Button>(R.id.btnminus)
-        var bttimes = findViewById<Button>(R.id.bttimes)
+        var btntimes = findViewById<Button>(R.id.btntimes)
         var btnmodulus = findViewById<Button>(R.id.btnmodulus)
 
 
                     btnsum.setOnClickListener {
                         var enter = etenter.text.toString().toInt()
                         var enters = etenters.text.toString().toInt()
-                        var  addition=enter-enters
-                        answer.setText(addition)
-                        //Toast.makeText(baseContext,addition, Toast.LENGTH_LONG).show()
+                        var addition = enter + enters
+                        answer.setText(addition.toString())
+                        //Toast.makeText(baseContext,enter, Toast.LENGTH_LONG).show()
+                    }
 
                         btnminus.setOnClickListener {
                             var enter = etenter.text.toString().toInt()
-                            var enters= etenters.text.toString().toInt()
-                            var subtraction=enter-enters
-                            answer.setText(subtraction)
-                         //Toast.makeText(baseContext,subtraction, Toast.LENGTH_LONG).show()
+                            var enters = etenters.text.toString().toInt()
+                            var subtraction = enter - enters
+                            answer.setText(subtraction.toString())
+                            //Toast.makeText(baseContext,enter, Toast.LENGTH_LONG).show()
 
-
-                   bttimes.setOnClickListener {
+                        }
+                   btntimes.setOnClickListener {
                        var enter = etenter.text.toString().toInt()
                        var enters = etenters.text.toString().toInt()
-                       var multiplication=enter*enters
-                       answer.setText(multiplication)
-                       //Toast.makeText(baseContext,multiplication, Toast.LENGTH_LONG).show()
-
+                       var multiplication = enter * enters
+                       answer.setText(multiplication.toString())
+                       //Toast.makeText(baseContext,enters, Toast.LENGTH_LONG).show()
+                   }
                    btnmodulus.setOnClickListener {
                        var enter = etenter.text.toString().toInt()
                        var enters = etenters.text.toString().toInt()
                        var modulus=enter%enters
-                       answer.setText(addition)
-                       //Toast.makeText(baseContext,modulus, Toast.LENGTH_LONG).show()
-                   }
-                   }
-                   }
-                    }
+                       answer.setText(modulus.toString())
+                      // Toast.makeText(baseContext,enters, Toast.LENGTH_LONG).show()
+
     }
-}
+}}
 
